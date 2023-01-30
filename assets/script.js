e885d588a3e66ae25b3de6166064c147584e2c53
khyleflores <khyleflores10@gmail.com>
2023-01-30 16:05:37 +0000

    Icon for today's weather has been added

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,12 @@
 //QUERY URL from API Weather Forecast using longitude and latitude - DONE
 //Store in variables
     //Current conditions
-        //The city name
-        //The date
-        //An icon representation of weather conditions
-        //The temperature
-        //The humidity
-        //The wind speed
+        //The city name DONE
+        //The date DONE
+        //An icon representation of weather conditions 
+        //The temperature DONE
+        //The humidity DONE
+        //The wind speed DONE (maybe convert to kph if needed)
     //Future conditions - 5days
         //The date
         //An icon representation of weather conditions
@@ -48,7 +48,7 @@ function getWeather(){
 
         // Constructing a queryURL using the city name
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" +
-        lat + "&lon=" + lon + "&appid=a3870bc5f9f6b6036fee3bdf6b81ac04";
+        lat + "&lon=" + lon + "&units=metric&appid=a3870bc5f9f6b6036fee3bdf6b81ac04";
 
         $.ajax({
             url: queryURL,
@@ -56,13 +56,15 @@ function getWeather(){
         })
         .then(function(response){
             console.log(response);
-
+            var date = moment(response.list[0].dt_txt).format("DD/MM/YYYY");
             var cityResult = response.city.name;
-            var tempResult = calculateCelsius(response.list[0].main.temp) + "C";
+            var weatherIcon = response.list[0].weather[0].icon;
+            console.log(weatherIcon);
+            var tempResult = response.list[0].main.temp + "C";
             var windResult = response.list[0].wind.speed;
             var humidityResult = response.list[0].main.humidity;
 
-            updateTodayDisplay(cityResult, tempResult, windResult, humidityResult)
+            updateTodayDisplay(cityResult, date, weatherIcon, tempResult, windResult, humidityResult)
         });
     });
 }
@@ -74,22 +76,27 @@ $("#search-button").on("click", function(event){
 
 });
 
-function updateTodayDisplay(cityResult, tempResult, windResult, humidityResult){
+function updateTodayDisplay(cityResult, dateResult, weatherIconResult, tempResult, windResult, humidityResult){
+    //Clear the data in the today forecast div in case there is a search from previous
+    $("#today").empty();
     var container = $("<div>");
     container.attr("class", "todayContainer")
-    cityName = $("<h2>");
-    cityName.html(cityResult);
+    var cityHeadline = $("<div>");
+    var cityName = $("<h2>");
+    var iconURL = "http://openweathermap.org/img/wn/" + weatherIconResult + "@2x.png";
+    var iconWeather = $("<img>");
+    iconWeather.attr("src", iconURL);
+    cityName.html(cityResult + " (" + dateResult + ")");
+    cityName.append(iconWeather);
+    cityHeadline.append(cityName);
     temp = $("<p>");
     temp.html("Temp: " + tempResult);
     wind = $("<p>");
     wind.html("Wind: " + windResult + "m/s");
     humidity = $("<p>");
     humidity.html("Humidity: " + humidityResult + "%");
-    container.append(cityName).append(temp).append(wind).append(humidity);
+    //Append all the elements to div container
+    container.append(cityHeadline).append(temp).append(wind).append(humidity);
+    //Append container div to today div
     $("#today").append(container);
-}
-
-function calculateCelsius(kelvinTemp){
-    tempResult = parseFloat((kelvinTemp - 273.15).toFixed(2));
-    return tempResult;
 }
